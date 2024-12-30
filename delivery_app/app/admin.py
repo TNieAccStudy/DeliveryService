67208d3b7b8accdb6f504a44ec53b3e17da51d48
TNieAccStudy <tniestudyacc176@gmail.com>
2024-12-30 22:07:30 +0700

    update delivery to final

--- delivery_app/app/admin.py
+++ delivery_app/app/admin.py
@@ -1,7 +1,7 @@
 from flask_admin import Admin
 from app import app,db
 from flask_admin.contrib.sqla import ModelView
-from app.models import Package, BaseUrl
+from app.models import Package, BaseUrl, PackageState
 import requests
 
 admin = Admin(app=app)
@@ -22,17 +22,20 @@ class PackageModelView(ModelView):
         if not is_created:
             if 'state' in form:
                 if model.state.name != "DELIVERING":
-                    url = get_base_url() + "/api/test"
+                    url = get_base_url() + f'/api/orders/{model.order_id}'
                     body = {
-                        'order_id' : model.order_id,
-                        'state' : model.state.name,
+                        'deliveryState' : model.state.name,
                     }
                     headers = {
                         "Content-Type": 'application/json',
                     }
-                    requests.post(url,json=body,headers=headers)
+                    response = requests.patch(url,json=body,headers=headers)
+                    print(response.status_code)
                 
         return super().after_model_change(form, model, is_created)
+    
+    # def get_query(self):
+    #     return Package.query.filter(Package.state==PackageState.DELIVERING)
     
 
 class BaseUrlModelView(ModelView):
