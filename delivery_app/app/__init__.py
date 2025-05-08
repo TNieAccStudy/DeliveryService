d025f763249ad057367074fbab5d678248548538
TNieAccStudy <tniestudyacc176@gmail.com>
2025-05-08 12:10:52 +0700

    change db host

--- delivery_app/app/__init__.py
+++ delivery_app/app/__init__.py
@@ -7,8 +7,8 @@ import secrets
 
 
 app = Flask("Delivery Service")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/deliverydb" % quote('Admin@123')
+db_host="terraform-20250508045456317100000001.cpm02wggy33q.us-east-1.rds.amazonaws.com"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://root:%s@{db_host}/deliverydb" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app=app)
